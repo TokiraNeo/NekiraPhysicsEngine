b524af8844a993975218f68b032ed9d758b78052
TokiraNeo <TokiraNeo@outlook.com>
2025-08-10 20:16:02 +0800

    Init

---
+++ Engine/Core/Private/Core.cpp
@@ -0,0 +1,7 @@
+﻿/**
+* MIT License
+ *
+ * Copyright (c) 2025 TokiraNeo (https://github.com/TokiraNeo)
+ *
+ * For more detail, please refer to the LICENSE file in the root directory of this project.
+ */
